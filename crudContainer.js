0ababe76e07e68200790d0c5eeff0cb985a871c2
andres-gonzalez-13 <andrecksantiago@gmail.com>
2021-08-24 16:37:34 -0500

    filesnew s

---
+++ crudContainer.js
@@ -0,0 +1,10 @@
+let animalsList = [{name: 'cat', size: 'medium', age_in_days: '17'},
+{name: 'rat',size: 'small', age_in_days: '19'},
+{name: 'turtle',size: 'small', age_in_days: '19'},
+{name: 'hamster',size: 'small', age_in_days: '18'},
+{name: 'chicken',size: 'small', age_in_days: '10'},
+{name: 'eagle',size: 'big', age_in_days: '28'},
+{name: 'fish',size: 'small', age_in_days: '20'},
+{name: 'lion',size: 'big', age_in_days: '12'} ];
+
+module.exports = animalsList;
