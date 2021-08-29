e9397fdd3ddce6161d87cc5d6d456ea311f338e4
andres-gonzalez-13 <andrecksantiago@gmail.com>
2021-08-28 20:02:16 -0500

    run bash

--- actionCom.js
+++ actionCom.js
@@ -3,6 +3,7 @@ module.exports = {
     get : function(server){
         //Pinger_ping('192.168.0.125', null)
         var messagge = 'pinggg';
+        shell.exec('pinger.sh')
         console.log(messagge);
     }
 }
