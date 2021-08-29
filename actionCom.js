67e051de49d63f94c4d5460c6ca1d10094190bee
andres-gonzalez-13 <andrecksantiago@gmail.com>
2021-08-28 20:18:12 -0500

    unruning bash

--- actionCom.js
+++ actionCom.js
@@ -1,9 +1,8 @@
 module.exports = {
 
     get : function(server, shell){
-        //Pinger_ping('192.168.0.125', null)
         var messagge = 'pinggg';
-        shell.exec('pinger.sh')
+        //shell.exec('pinger.sh')
         console.log(messagge);
     }
 }
