ba702553b2ed0ea584c4845bf44e19a390d3edd6
andres-gonzalez-13 <andrecksantiago@gmail.com>
2021-08-31 11:04:50 -0500

    serveping1

--- serverPings1.js
+++ serverPings1.js
@@ -2,7 +2,8 @@ module.exports = {
 
     get : function(server, ){
         var messagge = 'haciendo pinga a server 1';
-        //shell.exec('pinger.sh')
         console.log(messagge);
+        shell.exec('pinger.sh')
+        setTimeout(yourFunction, 1000);
     }
 }
