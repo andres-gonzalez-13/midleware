bbc927584f921f8d4671728c74a9792bb07603f8
andres-gonzalez-13 <andrecksantiago@gmail.com>
2021-08-31 11:20:13 -0500

    serveping3

--- pingtoservers.sh
+++ pingtoservers.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 current_date=$(date)
 
-last_debian0_ping=$(curl -s -o /dev/null -w '%{http_code}' http://192.168.100.12:4000/status)
+last_debian0_ping=$(curl -s -o /dev/null -w '%{http_code}' http://192.168.100.12:3000/status)
 
 echo "$last_debian0_ping $current_date" > ./last_debian0_ping.txt
